200a8ae2f14610c5dfb1cb0cb574a8fc76d9521b
Khaled Garbaya <khaled@contentful.com>
2017-09-26 15:24:12 +0200

    feat(courses): Add Categories listing

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,6 @@ const router = express.Router()
 /* GET home page. */
 router.get('/', async function (req, res, next) {
   const landingPage = await getLandingPage()
-  console.log(landingPage.fields.contentModules[1].fields)
   res.render('index', { landingPage })
 })
 
