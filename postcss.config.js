3e4f8865aa0fa0108c2383192d55f26658682576
Benedikt Rötsch <info@benedikt-roetsch.de>
2017-09-28 10:29:33 +0200

    use real BEM almost everywhere. Some restructuring is still missing

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: {
     'postcss-import': {},
     'postcss-url': {},
+    'postcss-atrule-bem': {},
     'postcss-cssnext': {},
     'postcss-browser-reporter': {},
     'postcss-reporter': {}
