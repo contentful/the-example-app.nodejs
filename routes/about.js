03edefa036424ad0e2cae4e0aab7919db305fbe6
Benedikt Rötsch <axe312ger@users.noreply.github.com>
2017-10-10 11:33:03 +0200

    feat(controls): styled custom header dropdown with helptext and smart positioning (#13)

--- routes/about.js
+++ routes/about.js
@@ -5,7 +5,7 @@ const router = express.Router()
 
 /* GET the about landing page. */
 router.get('/', catchErrors(async function (req, res, next) {
-  const landingPage = await getLandingPage('about', req.query.locale, req.query.api)
+  const landingPage = await getLandingPage('about', res.locals.currentLocale.code, res.locals.currentLocale.id)
   res.render('landingPage', { title: 'About', landingPage })
 }))
 
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const router = express.Router()
 
 /* GET the home landing page. */
 router.get('/', catchErrors(async function (req, res, next) {
-  const landingPage = await getLandingPage('home', req.query.locale, req.query.api)
+  const landingPage = await getLandingPage('home', res.locals.currentLocale.code, res.locals.currentLocale.id)
   let title = landingPage.fields.title
   if (!title || landingPage.fields.slug === 'home') {
     title = 'The Example App'
