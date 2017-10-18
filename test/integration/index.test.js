d7654bb9bf0ba3cb37841b89a985614348f37fce
Khaled Garbaya <khaled@contentful.com>
2017-10-18 15:29:18 +0200

    test: add integration test

---
+++ test/integration/index.test.js
@@ -0,0 +1,9 @@
+/* global describe, test */
+const app = require('../../app')
+const request = require('supertest')
+
+describe('Home page', () => {
+  test('it should render the landing page', () => {
+    return request(app).get('/').expect(200)
+  })
+})
