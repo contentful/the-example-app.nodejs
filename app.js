1e94188333ba1738a946d88eb6f01b5a6fc37d08
Khaled Garbaya <khaled@contentful.com>
2017-10-23 14:40:20 +0200

    refactor: refactor routes

--- app.js
+++ app.js
@@ -7,12 +7,7 @@ const logger = require('morgan')
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
 
-const index = require('./routes/index')
-const courses = require('./routes/courses')
-const categories = require('./routes/categories')
-const about = require('./routes/about')
-const settings = require('./routes/settings')
-const sitemap = require('./routes/sitemap')
+const routes = require('./routes/index')
 
 const { initClient, getSpace } = require('./services/contentful')
 const breadcrumb = require('./lib/breadcrumb')
@@ -36,7 +31,7 @@ app.use(async function (req, res, next) {
     space: process.env.CF_SPACE,
     cda: process.env.CF_ACCESS_TOKEN,
     cpa: process.env.CF_PREVIEW_ACCESS_TOKEN,
-    editorialFeatures: false,
+    editorialFeatures: true,
     ...req.cookies.theExampleAppSettings
   }
 
@@ -108,12 +103,7 @@ app.use(async function (req, res, next) {
   next()
 })
 
-app.use('/', index)
-app.use('/courses', courses)
-app.use('/categories', categories)
-app.use('/about', about)
-app.use('/settings', settings)
-app.use('/sitemap', sitemap)
+app.use('/', routes)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/categories.js
+++ routes/categories.js
@@ -1,10 +1,5 @@
-const express = require('express')
-const { catchErrors } = require('../handlers/errorHandlers')
-const router = express.Router()
-
 /* GET category listing. */
-router.get('/', catchErrors(async function (req, res, next) {
+exports.getCategories = async (req, res, next) => {
   res.render('categories', { title: 'Categories' })
-}))
+}
 
-module.exports = router
