c89bca0e4a220c9367823e3b720ff4dec411e165
Benedikt Rötsch <benedikt@contentful.com>
2017-10-24 14:55:23 +0200

    fixup! feat(entry-states): display entity state for editors

--- routes/courses.js
+++ routes/courses.js
@@ -1,10 +1,17 @@
 const {getCourses, getCourse, getCategories, getCoursesByCategory} = require('./../services/contentful')
+const attachEntryState = require('./../lib/entry-state')
 
 exports.getCourses = async (req, res, next) => {
   // we get all the entries with the content type `course`
   let courses = []
   let categories = []
   courses = await getCourses(res.locals.currentLocale.code, res.locals.currentApi.id)
+
+  // Attach entry state flags when using preview API
+  if (res.locals.settings.editorialFeatures && res.locals.currentApi.id === 'cpa') {
+    courses = await Promise.all(courses.map(attachEntryState))
+  }
+
   categories = await getCategories(res.locals.currentLocale.code, res.locals.currentApi.id)
   res.render('courses', { title: `All Courses (${courses.length})`, categories, courses })
 }
@@ -24,35 +31,9 @@ exports.getCourse = async (req, res, next) => {
   visitedLessons = [...new Set(visitedLessons)]
   res.cookie('visitedLessons', visitedLessons, { maxAge: 900000, httpOnly: true })
 
-  if (res.locals.settings.editorialFeatures) {
-    let entryDelivery, entryPreview
-
-    if (res.locals.currentApi.id === 'cda') {
-      entryDelivery = course
-      try {
-        entryPreview = await getCourse(req.params.slug, res.locals.currentLocale.code, 'cpa')
-      } catch (err) {
-        entryPreview = null
-      }
-    } else {
-      entryPreview = course
-      try {
-        entryDelivery = await getCourse(req.params.slug, res.locals.currentLocale.code, 'cda')
-      } catch (err) {
-        entryDelivery = null
-      }
-    }
-
-    return res.render('course', {
-      title: course.fields.title,
-      course,
-      lesson,
-      lessons,
-      lessonIndex,
-      visitedLessons,
-      entryDelivery,
-      entryPreview
-    })
+  // Get the published version of this course when using preview API for entry state detection
+  if (res.locals.settings.editorialFeatures && res.locals.currentApi.id === 'cpa') {
+    course = await attachEntryState(course)
   }
 
   res.render('course', {title: course.fields.title, course, lesson, lessons, lessonIndex, visitedLessons})
@@ -78,13 +59,21 @@ exports.getLesson = async (req, res, next) => {
   let course = await getCourse(req.params.cslug, res.locals.currentLocale.code, res.locals.currentApi.id)
   const lessons = course.fields.lessons
   const lessonIndex = lessons.findIndex((lesson) => lesson.fields.slug === req.params.lslug)
-  const lesson = lessons[lessonIndex]
+  let lesson = lessons[lessonIndex]
   const nextLesson = lessons[lessonIndex + 1] || null
+
+  // save visited lessons
   const cookie = req.cookies.visitedLessons
   let visitedLessons = cookie || []
   visitedLessons.push(lesson.sys.id)
   visitedLessons = [...new Set(visitedLessons)]
   res.cookie('visitedLessons', visitedLessons, { maxAge: 900000, httpOnly: true })
+
+  // Get the published version of this lesson when using preview API for entry state detection
+  if (res.locals.settings.editorialFeatures && res.locals.currentApi.id === 'cpa') {
+    lesson = await attachEntryState(lesson)
+  }
+
   res.render('course', {
     title: `${course.fields.title} | ${lesson.fields.title}`,
     course,
--- routes/landingPage.js
+++ routes/landingPage.js
@@ -1,14 +1,23 @@
 const { getLandingPage } = require('../services/contentful')
+const attachEntryState = require('./../lib/entry-state')
 const url = require('url')
 
 exports.getLandingPage = async (req, res, next) => {
   let pathname = url.parse(req.url).pathname.split('/').filter(Boolean)[0]
   pathname = pathname || 'home'
-  const landingPage = await getLandingPage(
+  let landingPage = await getLandingPage(
     pathname,
     res.locals.currentLocale.code,
     res.locals.currentApi.id
   )
+
+  // Get the published version of this lesson when using preview API for entry state detection
+  // let publishedLandingPage = null
+  if (res.locals.settings.editorialFeatures && res.locals.currentApi.id === 'cpa') {
+    // publishedLandingPage = await getPublishedEntry(landingPage)
+    landingPage = await attachEntryState(landingPage)
+  }
+
   res.render('landingPage', { title: pathname, landingPage })
 }
 
