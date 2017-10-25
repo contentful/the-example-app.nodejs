602b660417bc53333606056b57f5da07be84113e
Khaled Garbaya <khaled@contentful.com>
2017-10-25 16:40:06 +0200

    test: Add moare tests

--- test/unit/index.test.js
+++ test/unit/index.test.js
@@ -1,5 +1,7 @@
 /* global describe, test, beforeAll, afterEach, jest, expect */
 const { getCourses, getCourse, getCoursesByCategory, getLesson } = require('../../routes/courses')
+const { getSettings } = require('../../routes/settings')
+const { mockCourse, mockCategory } = require('./mocks/index')
 
 jest.mock('../../services/contentful')
 const contentful = require('../../services/contentful')
@@ -18,21 +20,9 @@ const res = {
 
 beforeAll(() => {
   contentful.getCourses.mockImplementation(() => [])
-  contentful.getCourse.mockImplementation(() => ({
-    sys: { id: 'id' },
-    fields: {
-      lessons: [
-        { sys: {id: 'lessonId'}, fields: { slug: 'lessonSlug' } }
-      ]
-    }
-  }))
+  contentful.getCourse.mockImplementation(() => mockCourse)
 
-  contentful.getCategories.mockImplementation(() => [
-    {
-      sys: { id: 'categoryId' },
-      fields: { slug: 'categorySlug', title: 'categoryTitle' }
-    }
-  ])
+  contentful.getCategories.mockImplementation(() => [mockCategory])
 
   contentful.getCoursesByCategory.mockImplementation(() => [])
   res.render = jest.fn()
@@ -62,6 +52,7 @@ describe('Courses', () => {
     req.params = {slug: 'slug', lslug: 'lslug', category: 'categorySlug'}
     await getCoursesByCategory(req, res)
     expect(res.render.mock.calls[0][0]).toBe('courses')
+    expect(res.render.mock.calls[0][1].title).toBe(`${mockCategory.fields.title} (0)`)
     expect(res.render.mock.calls.length).toBe(1)
   })
 })
@@ -71,6 +62,26 @@ describe('Lessons', () => {
     req.params = { cslug: 'courseSlug', lslug: 'lessonSlug' }
     await getLesson(req, res)
     expect(res.render.mock.calls[0][0]).toBe('course')
+    expect(res.render.mock.calls[0][1].title).toBe('Course title | Lesson title')
+    expect(res.render.mock.calls[0][1].course.sys.id).toBe(mockCourse.sys.id)
+    expect(res.render.mock.calls[0][1].lesson.sys.id).toBe(mockCourse.fields.lessons[0].sys.id)
     expect(res.render.mock.calls.length).toBe(1)
+  })
+})
+
+describe('Settings', () => {
+  test('It should render settings', async () => {
+    res.locals = {
+      settings: {
+        space: 'spaceId',
+        cda: 'cda',
+        cpa: 'cpa',
+        editorialFeatures: false
+      }
+    }
+    await getSettings(req, res)
+    expect(res.render.mock.calls[0][0]).toBe('settings')
+    expect(res.render.mock.calls[0][1].title).toBe('Settings')
+    expect(res.render.mock.calls[0][1].settings).toBe(res.locals.settings)
   })
 })
---
+++ test/unit/mocks/index.js
@@ -0,0 +1,24 @@
+const mockCourse = {
+  sys: { id: 'courseId' },
+  fields: {
+    title: 'Course title',
+    lessons: [
+      { sys: {id: 'lessonId'}, fields: { slug: 'lessonSlug', title: 'Lesson title' } }
+    ]
+  }
+}
+
+const mockCategory = {
+  sys: {
+    id: 'categoryId'
+  },
+  fields: {
+    slug: 'categorySlug',
+    title: 'categoryTitle'
+  }
+}
+
+module.exports = {
+  mockCourse,
+  mockCategory
+}
