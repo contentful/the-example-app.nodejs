a884ba2c6ebdbdb7cf7a024a56594b968a036cfa
Khaled Garbaya <khaled@contentful.com>
2017-10-30 10:28:13 +0100

    chore: Add comments to routes and services

--- routes/categories.js
+++ routes/categories.js
@@ -1,3 +1,7 @@
+/*
+ * The purpose of this module is to render the category page when the route is requested
+ */
+
 // GET category listing
 module.exports.getCategories = async (req, res, next) => {
   res.render('categories', { title: 'Categories' })
--- services/contentful.js
+++ services/contentful.js
@@ -1,9 +1,10 @@
+/**
+ * The purpose of this module is to get data from contentful
+ */
 const { createClient } = require('contentful')
 
 let deliveryClient = null
 let previewClient = null
-
-// Initialize our client
 
 /**
  * Initialize the contentful Client
