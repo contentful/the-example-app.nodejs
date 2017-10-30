14d96364df9d3ea03141e54056567095d7756b17
Mario Bodemann <mario.bodemann@gmail.com>
2017-10-30 13:54:51 +0100

    chore: removed unnecessary comment line sign

--- routes/settings.js
+++ routes/settings.js
@@ -1,7 +1,7 @@
 /**
  * This module renders the settings page when `settings` route is requested
  * Also save the settings to the cookies
- * */
+ */
 const { createClient } = require('contentful')
 const { initClient, getSpace } = require('./../services/contentful')
 
