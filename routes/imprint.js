bba56101ac179be046b7121745832d75134e5726
Mario Bodemann <mario.bodemann@gmail.com>
2017-10-30 13:49:52 +0100

    typo: removing t

--- routes/imprint.js
+++ routes/imprint.js
@@ -2,7 +2,7 @@
  * Renders imprint page when `/imprint` is route is requested
  * @param req - Object - Express request
  * @param res - Object - Express response
- * @paramt next - Function - Express callback
+ * @param next - Function - Express callback
  * @returns {undefined}
  */
 module.exports.getImprint = (req, res, next) => {
