f1384d08b82dd0f49b85339f4e82602af723d749
Khaled Garbaya <khaled@contentful.com>
2017-10-30 13:29:41 +0100

    chore: move cookie name to a constant

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const { initClient, getSpace } = require('./services/contentful')
 const app = express()
 
 const ONE_YEAR = 31536000
-
+const SETTINGS_NAME = 'theExampleAppSettings'
 // View engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
@@ -50,7 +50,7 @@ app.use(async function (req, res, next) {
       deliveryToken: delivery_token,
       previewToken: preview_token
     }
-    res.cookie('theExampleAppSettings', settings, { maxAge: ONE_YEAR, httpOnly: true })
+    res.cookie(SETTINGS_NAME, settings, { maxAge: ONE_YEAR, httpOnly: true })
   }
 
   // Allow enabling of editorial features via query parameters
@@ -58,7 +58,7 @@ app.use(async function (req, res, next) {
   if (enable_editorial_features !== undefined) { // eslint-disable-line camelcase
     delete req.query.enable_editorial_features
     settings.editorialFeatures = true
-    res.cookie('theExampleAppSettings', settings, { maxAge: ONE_YEAR, httpOnly: true })
+    res.cookie(SETTINGS_NAME, settings, { maxAge: ONE_YEAR, httpOnly: true })
   }
 
   initClient(settings)
