add61715ab4754c3da1a2be90d69a618007ffadd
Khaled Garbaya <khaled@contentful.com>
2017-11-02 15:27:31 +0100

    fixup! fixup! fixup! fixup! fixup! chore: Address Fredericks comments

--- handlers/errorHandlers.js
+++ handlers/errorHandlers.js
@@ -1,6 +1,5 @@
 /**
  * Catch Errors Handler
- * With async/await, you need some way to catch errors.
  * Instead of using try{} catch(e) {} in each controller, we wrap the function in
  * catchErrors(), catch any errors they throw, and pass it along to our express middleware with next().
  */
