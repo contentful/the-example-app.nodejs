751b8aaf7f35cf6fac7f32da897b0ba3077bedbd
Benedikt Rötsch <benedikt@contentful.com>
2017-11-06 14:56:45 +0100

    refactor(routes): remove unused categories route files

--- routes/categories.js
+++
@@ -1,9 +0,0 @@
-/*
- * The purpose of this module is to render the category page when the route is requested
- */
-
-// Renders categories page when `/categories` route is requested
-module.exports.getCategories = async (request, response, next) => {
-  response.render('categories', { title: 'Categories' })
-}
-
