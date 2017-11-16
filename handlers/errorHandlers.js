c944885446436a08b49bf3b7d020ae9cf9548216
Khaled Garbaya <khaledgarbaya@gmail.com>
2017-11-16 14:42:28 +0100

    Production deploy 16:00 (#70)
    * Production (#44)
    
    * test: Fix integration test
    
    * fix: Fix Editorial feature
    
    * chore(localization): move locales to i18n folder
    
    * feat(modules): add image caption and shadow
    
    * fix: clarify German translation🇩🇪
    
    * chore(localization): fix typo (#47)
    
    * Split first paragraph into sections (#46)
    
    * feat(controls): add help text to API dropdown
    
    * fix(style): simplify header and fix mobile design
    
    * fix(style): simplify footer and fix mobile design
    
    * fix(style): update stylesheet and scripts file
    
    * test(e2e): adjust tests and make them fail on CI
    
    * language fixes
    
    * rename labels to use full api name
    
    * Refactor entry state toggle (#48)
    
    * chore(editorialFeatures): extract editorial features toggle
    
    * chore(refactor): rename function to make more contextual sense
    
    * Fix breadcrumb and add test for locales (#49)
    
    * test(unit): add test to check for locale consistency
    
    * fix(breadcrumb): translate lessons route
    
    * Change readme url to http from https (#52)
    
    * tests(e2e): expect only 2 courses
    
    * fix(cookies): fix variable naming and extend to two days maxAge
    
    * Change text on next lesson button (#54)
    
    * Retranslate advancedLabel
    
    * fix(translations): do not break on vim temp files. This is dedicated to JP
    
    * fix(analytics): use correct app-id
    
    * Add instructions to seed a space to readme (#61)
    
    * Change modal text; add link to model's repo to localization files (#57)
    
    * fix(links): link correct GH repository
    
    * fix(i18n): adjust view on github link label
    
    * fix(style): move GitHub link to the left
    
    * new screenshot
    
    * Update README.md (#63)
    
    * Update README.md
    
    * Update README.md
    
    * Https mistake and Heroku buton
    
    * fixup
    
    * fixup
    
    * feat(heroku): add app.json
    
    * docs(README): add linebreak
    
    * docs(README): minor adjustments
    
    * fix(tracking): add link click tracking to snowplow
    
    * Add translation for edit in the web app (#64)
    
    * chore(typo): fix error message (#67)
    
    * chore(update): remove image rendering on lessons (#65)
    
    * chore: Improve error page (#66)
    
    * chore: Improve error page
    
    * fix: Fix middlware order
    
    * fix: Remove console.log
    
    * fix:  no more duplicate error messges (#71)
    
    * fix: Fix middlware order
    
    * fix: Remove console.log
    
    * feat: Uniq error meassages
    
    * fix: fix cli link (#72)
    
    * Add instructions to run app as Docker container (#68)

--- handlers/errorHandlers.js
+++ handlers/errorHandlers.js
@@ -7,6 +7,9 @@
 module.exports.catchErrors = (fn) => {
   return function (request, response, next) {
     return fn(request, response, next).catch((e) => {
+      if (e.response) {
+        e.status = e.response.status
+      }
       next(e)
     })
   }
