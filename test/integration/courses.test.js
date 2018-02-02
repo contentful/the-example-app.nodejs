e5f9768da0085fed464afbc27f3a8d61ca727aad
Benedikt Rötsch <benedikt@contentful.com>
2018-02-02 14:43:52 +0100

    test(integration): fix path to match new content

--- test/integration/courses.test.js
+++ test/integration/courses.test.js
@@ -22,7 +22,7 @@ describe('courses', () => {
   })
 
   test('it should render a lesson', () => {
-    return request(app).get('/courses/hello-contentful/lessons/architecture')
+    return request(app).get('/courses/hello-contentful/lessons/content-management')
       .expect(200)
       .then((response) => {
         expect(response.text.match(/class="lesson__title"/)).toBeTruthy()
