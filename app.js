cdbfa96426fd0ea24de6758497bc3b97929cd268
Benedikt Rötsch <benedikt@contentful.com>
2018-02-20 10:25:54 +0100

    fix(errors): only remove stack from error in production

--- app.js
+++ app.js
@@ -149,8 +149,11 @@ app.use(function (request, response, next) {
 // Error handler
 app.use(function (err, request, response, next) {
   // Set locals, only providing error in development
-  response.locals.error = request.app.get('env') === 'development' ? err : {}
+  response.locals.error = err
   response.locals.error.status = err.status || 500
+  if (request.app.get('env') !== 'development') {
+    delete err.stack
+  }
   response.locals.title = 'Error'
   // Render the error page
   response.status(err.status || 500)
