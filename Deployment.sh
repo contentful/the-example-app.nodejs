2c6fd516b45be38744ac7e9dee2bafccc2ad057e
subhamproject <subhammandal01@gmail.com>
2018-05-22 17:54:12 +0530

    Update Deployment.sh

--- Deployment.sh
+++ Deployment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
 cd $(dirname $0)
-TEMP_PATH="$WORKSPACE/tmp"
-[ -d $TEMP_PATH ] || mkdir -p $TEMP_PATH
+TEMP_DIR="$WORKSPACE/tmp"
+[ -d $TEMP_DIR ] || mkdir -p $TEMP_DIR
 FILES=$(git diff --name-only HEAD~1 HEAD|grep -v Deployment.sh)
 
 if [ -n "${FILES}" ];then
@@ -10,8 +10,8 @@ do
 file=$(find $WORKSPACE/ -name $I)
 if [ -n "${file}" ]
 then 
-echo "File \"${file}\" found,Copying to temp dir $TEMP_PATH!"
-sudo cp -pr $file $TEMP_PATH
+echo "File \"${file}\" found,Copying to temp dir $TEMP_DIR!"
+sudo cp -pr $file $TEMP_DIR
 elif [ -z "${file}" ];then
 echo "Could not find file \"$I\",As this file was deleted in last commit..Skipping!"
 fi
@@ -19,9 +19,9 @@ done
 else
 echo "No Changes were done in last commit!"
 fi
-cd $TEMP_PATH
+cd $TEMP_DIR
 [ `ls -1|wc -l` -ge 1 ] && sudo tar -zcvf /tmp/archive-name.tar.gz . --exclude="*.sh" >> /dev/null
 [ $? -eq 0 ] && sudo tar -zxvf /tmp/archive-name.tar.gz -C /tmp/mytest >> /dev/null
 sudo chown -R subham:subham /tmp/mytest >> /dev/null
-sudo rm -rf $TEMP_PATH
+sudo rm -rf $TEMP_DIR
 sudo rm -rf /tmp/archive-name.tar.gz
