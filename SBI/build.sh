42f539dbd38974002655dec19f8f783b0bb2393d
subhamproject <subhammandal01@gmail.com>
2018-05-23 16:20:59 +0530

    Update build.sh

--- SBI/build.sh
+++ SBI/build.sh
@@ -6,4 +6,4 @@ export GROUPID=$(id -g)
 echo "Running as UID=$USERID, GID=$GROUPID"
 cd $(dirname $0)
 
-docker-compose -f test-bed.yml run --rm -w "$WORKSPACE" --entrypoint "SBI/dockerize.sh" rcx-bi
+docker-compose -f test-bed.yml run --rm -w "$WORKSPACE" --entrypoint "SBI/test.sh" rcx-bi
