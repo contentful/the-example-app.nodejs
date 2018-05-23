75c67c7e402a85e1fe6ebe8920b48842470083e0
root <root@ip-172-31-1-36.ap-south-1.compute.internal>
2018-05-23 10:45:24 +0000

    dockerfile chaged

--- SBI/dockerize.sh
+++ SBI/dockerize.sh
@@ -1,30 +1,15 @@
-#!/bin/bash -xe
-
-TEMP_DIR="$(pwd)/tmp"
-TEMP_FILE="SBI/service-manifest.txt"
-[ -d $TEMP_DIR ] || mkdir -p $TEMP_DIR
-FILES=$(git diff --name-only HEAD~1 HEAD)
-
-echo $FILES > $TEMP_FILE
-
-if [ -n "${TEMP_FILE}" ];then
-  for I in $(cat "${TEMP_FILE}"|xargs -n1 basename)
- do
- file=$(find . -name $I)
-   if [ -n "${file}" ]
-   then
-   echo "File \"${file}\" found,Copying to temp dir $TEMP_DIR!"
-   cp -pr $file $TEMP_DIR
-   elif [ -z "${file}" ];then
-   echo "Could not find file \"$I\",As this file was deleted in last commit..Skipping!"
-   fi
- done
-else
-echo "No Changes were done in last commit!"
-fi
-cd $TEMP_DIR
-[ `ls -1|wc -l` -ge 1 ] &&  tar -zcvf /tmp/archive-name.tar.gz .  >> /dev/null
-cd - >> /dev/null
-[ $? -eq 0 ] && cp -pr /tmp/archive-name.tar.gz .
-
-cat /dev/null > $TEMP_FILE
+#!/bin/bash
+case $BRANCH_NAME in
+     master)
+     sh SBI/runtests.sh
+     ;;
+     develop)
+     sh SBI/runtest.sh
+     ;;
+     qa)
+     sh SBI/runtests.sh
+     *)
+      [ -z "${BRANCH_NAME} ] && echo "Branch is null"
+      exit 1
+      ;;
+esac
