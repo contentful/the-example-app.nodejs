ad03122dc10e55bf40bbc9764747c3d0fdb21c88
subhamproject <subhammandal01@gmail.com>
2018-05-23 16:53:37 +0530

    Update runtest.sh

--- SBI/runtest.sh
+++ SBI/runtest.sh
@@ -23,8 +23,7 @@ else
 echo "No Changes were done in last commit!"
 fi
 cd $TEMP_DIR
-[ `ls -1|wc -l` -ge 1 ] &&  tar -zcvf /tmp/archive-name.tar.gz .  >> dev/null
-cd - >> /dev/null
+[ `ls -1|wc -l` -ge 1 ] &&  tar -zcvf /tmp/archive-name.tar.gz .  >> /dev/null && cd - >> /dev/null
 [ $? -eq 0 ] && cp -pr /tmp/archive-name.tar.gz . >> /dev/null
 
 cat /dev/null > $TEMP_FILE
