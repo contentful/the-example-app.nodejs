eb740924cb81069476563965a90c58bab2070f83
root <root@ip-172-31-1-36.ap-south-1.compute.internal>
2018-05-23 07:44:19 +0000

    menifest changes

--- SBI/dockerize.sh
+++ SBI/dockerize.sh
@@ -1,11 +1,13 @@
 #!/bin/bash -xe
-#cd $(dirname $0)
 TEMP_DIR="$WORKSPACE/tmp"
+TEMP_FILE="$WORKSPACE/SBI/service-manifest.txt"
 [ -d $TEMP_DIR ] || mkdir -p $TEMP_DIR
 FILES=$(git diff --name-only HEAD~1 HEAD|grep -v Deployment.sh)
 
-if [ -n "${FILES}" ];then
-  for I in $(echo "${FILES}"|xargs -n1 basename)
+cat $FILES >> $TEMP_FILE
+
+if [ -n "${TEMP_FILE}" ];then
+  for I in $(echo "${TEMP_FILE}"|xargs -n1 basename)
  do
  file=$(find . -name $I)
    if [ -n "${file}" ]
@@ -23,7 +25,5 @@ cd $TEMP_DIR
 [ `ls -1|wc -l` -ge 1 ] &&  tar -zcvf /tmp/archive-name.tar.gz .  >> /dev/null
 cd -
 [ $? -eq 0 ] && cp -pr /tmp/archive-name.tar.gz .
-#[ $? -eq 0 ] &&  tar -zxvf /tmp/archive-name.tar.gz -C /tmp/mytest >> /dev/null
-#chown -R subham:subham /tmp/mytest >> /dev/null
-#rm -rf $TEMP_DIR
-#rm -rf /tmp/archive-name.tar.gz
+
+cat /dev/null > $TEMP_FILE
