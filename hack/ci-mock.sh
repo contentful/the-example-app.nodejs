2a0b730e0b10d17021042bd2cd68280d0067f223
kunrad <duruconrad@gmail.com>
2020-06-26 09:12:50 +0100

    update on hack

--- hack/ci-mock.sh
+++ hack/ci-mock.sh
@@ -29,6 +29,6 @@ fi
 
 echo ">>>> Building image ${image} <<<<"
 
-docker build --build-arg GITCOMMIT=${commit} --build-arg VERSION=${version} -t ${image} -f Dockerfile.ci .
+docker build --build-arg GITCOMMIT=${commit} --build-arg VERSION=${version} -t ${image} -f ../Dockerfile .
 
 docker push ${image}
