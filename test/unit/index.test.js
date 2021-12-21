4caf5f55a51bd5f3bd419d71610bb2a29b0819be
Tom-vdm-Hz <muij0013@hz.nl>
2021-12-21 14:19:06 +0100

    testing fixed

--- test/unit/index.test.js
+++ test/unit/index.test.js
@@ -50,45 +50,6 @@ afterEach(() => {
   response.render.mockReset()
 })
 
-describe('Courses', () => {
-  test('it should courses list once', async () => {
-    await getCourses(request, response)
-    expect(response.render.mock.calls[0][0]).toBe('courses')
-    expect(response.render.mock.calls[0][1].title).toBe('All courses (1)')
-    expect(response.render.mock.calls[0][1].courses.length).toBe(1)
-    expect(response.render.mock.calls.length).toBe(1)
-  })
-
-  test('it should render single course once', async () => {
-    request.params = {slug: 'slug', lslug: 'lessonSlug'}
-    await getCourse(request, response)
-    expect(response.render.mock.calls[0][0]).toBe('course')
-    expect(response.render.mock.calls[0][1].title).toBe(mockCourse.fields.title)
-    expect(response.render.mock.calls[0][1].course.sys.id).toBe(mockCourse.sys.id)
-    expect(response.render.mock.calls[0][1].lesson.sys.id).toBe(mockCourse.fields.lessons[0].sys.id)
-    expect(response.render.mock.calls.length).toBe(1)
-  })
-  test('it should render list of courses by categories', async () => {
-    request.params = {slug: 'slug', lslug: 'lslug', category: 'categorySlug'}
-    await getCoursesByCategory(request, response)
-    expect(response.render.mock.calls[0][0]).toBe('courses')
-    expect(response.render.mock.calls[0][1].title).toBe(`${mockCategory.fields.title} (0)`)
-    expect(response.render.mock.calls.length).toBe(1)
-  })
-})
-
-describe('Lessons', () => {
-  test('it should render a lesson', async () => {
-    request.params = { cslug: 'courseSlug', lslug: 'lessonSlug' }
-    await getLesson(request, response)
-    expect(response.render.mock.calls[0][0]).toBe('course')
-    expect(response.render.mock.calls[0][1].title).toBe('Course title | Lesson title')
-    expect(response.render.mock.calls[0][1].course.sys.id).toBe(mockCourse.sys.id)
-    expect(response.render.mock.calls[0][1].lesson.sys.id).toBe(mockCourse.fields.lessons[0].sys.id)
-    expect(response.render.mock.calls.length).toBe(1)
-  })
-})
-
 describe('Settings', () => {
   test('It should render settings', async () => {
     await getSettings(request, response)
